30b7e6355ce200f69b565cee9c67c5ad2a402916
Алексей <97665299+0ne290@users.noreply.github.com>
2025-01-14 18:57:53 +0800

    Update Windows_Build.ps1

--- Windows_Build.ps1
+++ Windows_Build.ps1
@@ -4,7 +4,7 @@ if ( $args[0].length -ne 0 )
 	{
 		Remove-Item $($args[0] + "/*") -Force -Recurse -Confirm:$false
 	}
-	dotnet publish ConsoleApplication/ConsoleApplication.csproj --configuration Release --runtime win-x64 --self-contained true --framework net8.0 --output $args[0] -p:PublishReadyToRun=true
+	dotnet publish ConsoleApplication/ConsoleApplication.csproj --configuration Release --runtime win-x64 --self-contained false --framework net8.0 --output $args[0] -p:PublishReadyToRun=true -p:PublishSingleFile=true
 }
 else
 {
