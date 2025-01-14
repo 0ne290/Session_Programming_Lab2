2b42d71ddc0225ac60b8d18f2f09759305f1bbac
Алексей <97665299+0ne290@users.noreply.github.com>
2025-01-14 17:16:22 +0800

    Update and rename Windows_BuildServer.ps1 to Windows_Build.ps1

--- Windows_Build.ps1
+++ Windows_Build.ps1
@@ -5,7 +5,7 @@ if ( $args[0].length -ne 0 )
 		Remove-Item $($args[0] + "/*") -Force -Recurse -Confirm:$false
 	}
 	dotnet publish WebApi/WebApi.csproj --configuration Release --runtime win-x64 --self-contained true --framework net8.0 --output $args[0] -p:PublishReadyToRun=true
-	Copy-Item "WebApi/certificate.pfx" -Destination $args[0]
+	Copy-Item "Books.txt" -Destination $args[0]
 }
 else
 {
